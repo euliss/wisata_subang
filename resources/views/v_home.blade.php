9c814df4fe524e92720e773ec0c10194e77de572
prawamayoza <prawamayoza@gmail.com>
2023-06-16 03:54:11 +0700

    fixing

--- resources/views/v_home.blade.php
+++ resources/views/v_home.blade.php
@@ -1,129 +1,126 @@
 @section('title')
-Dashboard
+    Dashboard
 @endsection
 @extends('layout/v_template')
 @section('page')
-Dashboard Admin
+    Dashboard Admin
 @endsection
 @section('content')
-<main id="main" class="main">
+    <main id="main" class="main">
 
-  <div class="pagetitle">
-    <h1>@yield('title')</h1>
-    <nav>
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="#">@yield('title')</a></li>
-        <li class="breadcrumb-item active">@yield('page')</li>
-      </ol>
-    </nav>
-  </div><!-- End Page Title -->
+        <div class="pagetitle">
+            <h1>@yield('title')</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="#">@yield('title')</a></li>
+                    <li class="breadcrumb-item active">@yield('page')</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
 
-  <section class="section dashboard">
-    <div class="row">
+        <section class="section dashboard">
+            <div class="row">
 
-      <!-- Left side columns -->
-      <div class="col-lg-9">
-        <div class="row">
-
-          <!-- Sales Card -->
-          <div class="col-xxl-4 col-md-6">
-            <div class="card info-card sales-card">
-
-              <div class="card-body">
-                <h5 class="card-title">Number of Users</h5>
-
-                <div class="d-flex align-items-center">
-                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-people"></i>
-                  </div>
-                  <div class="ps-3">
-                    <h6>{{ $users }}</h6>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-          <div class="col-xxl-4 col-md-6">
-            <div class="card info-card sales-card">
-
-              <div class="card-body">
-                <h5 class="card-title">Number of Categories</h5>
-
-                <div class="d-flex align-items-center">
-                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-intersect"></i>
-                  </div>
-                  <div class="ps-3">
-                    <h6>{{ $categories }}</h6>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-          <div class="col-xxl-4 col-md-6">
-            <div class="card info-card sales-card">
-
-              <div class="card-body">
-                <h5 class="card-title">Number of Destinations</h5>
-
-                <div class="d-flex align-items-center">
-                  <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                    <i class="bi bi-layers"></i>
-                  </div>
-                  <div class="ps-3">
-                    <h6>{{ $destinations }}</h6>
-                  </div>
-                </div>
-              </div>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-          </div>
-          <!-- Reports -->
-          <div class="col-6 ">
-            <div class="card">
-              <div class="card-body pt-4">
-                <h5>Grafik Jumlah Pengunjung Per Bulan</h5>
-                <canvas style="border-radius: 6px;" id="chartReport"></canvas><br><br>
-              </div>
-            </div>
-          </div>
-          <div class="col-12">
-            <div class="card">
-
-              <div class="card-body">
+                <!-- Left side columns -->
                 <div class="row">
-                  <div class="col-xxl-8 col-md-6 d-flex align-items-center justify-content-center ">
-                    <img src="{{ asset('layout') }}/assets/img/wisata.png" alt="Logo" width="500px">
-                  </div>
-                  <div class="col-xxl-4 col-md-6">
-                    <div class="info-card sales-card">
 
+                    <!-- Sales Card -->
+                    <div class="col-xxl-4 col-md-4">
+                        <div class="card info-card sales-card">
 
-                  <div class="col-xxl-12 col-md-12">
-                    <h3><strong>Welcome {{Auth::user()->name}}</strong></h3>
-                    <h5 >Welcome to the website of Destinasi Wisata. You are currently on the website of Destinasi Wisata, Admin section.</h5>
-                  </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Number of Users</h5>
 
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-people"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{ $users }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-md-4">
+                        <div class="card info-card sales-card">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Number of Categories</h5>
+
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-intersect"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{ $categories }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xxl-4 col-md-4">
+                        <div class="card info-card sales-card">
+
+                            <div class="card-body">
+                                <h5 class="card-title">Number of Destinations</h5>
+
+                                <div class="d-flex align-items-center">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-layers"></i>
+                                    </div>
+                                    <div class="ps-3">
+                                        <h6>{{ $destinations }}</h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-
             </div>
-          </div><!-- End Reports -->
-        </div>
-      </div><!-- End Left side columns -->
+            <!-- Reports -->
+            <div class="col-6 ">
+                <div class="card">
+                    <div class="card-body pt-4">
+                        <h5>Grafik Jumlah Pengunjung Per Bulan</h5>
+                        <canvas style="border-radius: 6px;" id="chartReport"></canvas><br><br>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12">
+                <div class="card">
 
-      <!-- Right side columns -->
-      <div class="col-lg-3">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-xxl-8 col-md-6 d-flex align-items-center justify-content-center ">
+                                <img src="{{ asset('layout') }}/assets/img/wisata.png" alt="Logo" width="500px">
+                            </div>
+                            <div class="col-xxl-4 col-md-6">
+                                <div class="info-card sales-card">
 
-        <!-- Recent Activity -->
-        <div class="card">
 
-          {{-- <div class="card-body">
+                                    <div class="col-xxl-12 col-md-12">
+                                        <h3><strong>Welcome {{ Auth::user()->name }}</strong></h3>
+                                        <h5>Welcome to the website of Destinasi Wisata. You are currently on the website of
+                                            Destinasi Wisata, Admin section.</h5>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div><!-- End Reports -->
+                </div>
+            </div><!-- End Left side columns -->
+
+            <!-- Right side columns -->
+            <div class="col-lg-3">
+
+                <!-- Recent Activity -->
+                <div class="card">
+
+                    {{-- <div class="card-body">
             <h5 class="card-title">Latest News</h5>
 
             <div class="activity">
@@ -140,53 +137,48 @@ Dashboard Admin
             </div>
 
           </div> --}}
-        </div><!-- End Recent Activity -->
-      </div><!-- End Right side columns -->
+                </div><!-- End Recent Activity -->
+            </div><!-- End Right side columns -->
 
-    </div>
-  </section>
+            </div>
+        </section>
 
-</main>
+    </main>
 @endsection
 
 @section('javascript')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
 
-<script>
+    <script>
+        function getChart() {
+            Chart.defaults.global.defaultFontSize = 16;
 
-    function getChart(){
-        Chart.defaults.global.defaultFontSize = 16;
-
-        var myBarChart = new Chart(document.getElementById("chartReport").getContext('2d'), {
-            type: 'bar',
-            data: {
-                labels: [
-                    <?php foreach($graphic as $val): ?>
-                        "<?= date("F", strtotime(date("Y-".$val->month."-d"))) ?>",
-                    <?php endforeach ?>
-                ],
-                datasets: [
-                {
-                    label : "Chart",
-                    backgroundColor: ['#ff6','#f6f','#6ff','#66f','#f66',],
-                    data: [
-                      <?php foreach($graphic as $val): ?>
-                          <?= $val->report_count ?>,
-                      <?php endforeach ?>
+            var myBarChart = new Chart(document.getElementById("chartReport").getContext('2d'), {
+                type: 'bar',
+                data: {
+                    labels: [
+                        <?php foreach($graphic as $val): ?> "<?= date('F', strtotime(date('Y-' . $val->month . '-d'))) ?>",
+                        <?php endforeach ?>
                     ],
+                    datasets: [{
+                        label: "Chart",
+                        backgroundColor: ['#ff6', '#f6f', '#6ff', '#66f', '#f66', ],
+                        data: [
+                            <?php foreach($graphic as $val): ?>
+                            <?= $val->report_count ?>,
+                            <?php endforeach ?>
+                        ],
 
+                    }, ]
                 },
-                ]
-            },
-            options: {
-              legend: {
-            display: false
-         }
-            }
-        });
-    }
+                options: {
+                    legend: {
+                        display: false
+                    }
+                }
+            });
+        }
 
-    getChart()
-
-</script>
+        getChart()
+    </script>
 @endsection
