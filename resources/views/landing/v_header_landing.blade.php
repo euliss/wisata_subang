d071e1615e28ee398d2e09a76f695bc3c1cf5e8c
euliss <eulisjubaedah08@gmail.com>
2023-06-19 16:32:34 +0700

    fixing update frontend

--- resources/views/landing/v_header_landing.blade.php
+++ resources/views/landing/v_header_landing.blade.php
@@ -91,76 +91,35 @@
       </div>
     </nav>
     
-    
-    
-
-
+  
     @yield('content')
+
 
     <footer class="ftco-footer ftco-section">
       <div class="container">
-        <div class="row mb-5">
+        <div class="row mb-3">
           <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">About <span><a href="index.html">Ecoland</a></span></h2>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-              </ul>
+            <div class="ftco-footer-widget mb-2">
+              <img src="{{ asset('ecoland/images/logo-white.png')}}" height="80px" alt="Logo"/></a>
+              <p> Created by :</p>
+              <p> Dinas Pariwisata Pemuda dan Olahraga Kabupaten Subang  </p>
+              <p> Informasi Lebih Lanjut :</p>
+              <a href="https://wa.me/085863518629" class="btn btn-success"><i class="ion-logo-whatsapp"></i> Hubungi Kami</a>
             </div>
           </div>
           <div class="col-md">
             <div class="ftco-footer-widget mb-4 ml-md-4">
-              <h2 class="ftco-heading-2">Information</h2>
-              <ul class="list-unstyled">
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Online Enquiry</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>General Enquiry</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Booking</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Privacy</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Refund Policy</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Call Us</a></li>
-              </ul>
+              <p>Lokasi :</p>
+              <p>Jl. Jend. Achmad Yani No.11, Pasirkareumbi, Kec. Subang Kabupaten Subang Jawa Barat</p>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3963.5801145580176!2d107.75470330968386!3d-6.574555993391404!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e693cbc83e65e4f%3A0x1d6f46b5875bd277!2sDinas%20Kebudayaan%20Pariwisata%20Pemuda%20Dan%20Olah%20Raga!5e0!3m2!1sid!2sid!4v1687158543166!5m2!1sid!2sid" width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
             </div>
           </div>
-          <div class="col-md">
-             <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Experience</h2>
-              <ul class="list-unstyled">
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Adventure</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Hotel and Restaurant</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Beach</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Nature</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Camping</a></li>
-                <li><a href="#"><span class="icon-long-arrow-right mr-2"></span>Party</a></li>
-              </ul>
-            </div>
-          </div>
-          <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-              <h2 class="ftco-heading-2">Have a Questions?</h2>
-              <div class="block-23 mb-3">
-                <ul>
-                  <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                  <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                  <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@yourdomain.com</span></a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-12 text-center">
-  
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
           </div>
         </div>
       </div>
     </footer>
-    
+
   
 
   <!-- loader -->
@@ -171,15 +130,15 @@
   <script src="{{ asset('ecoland/js/jquery-migrate-3.0.1.min.js') }}"></script>
   <script src="{{ asset('ecoland/js/popper.min.js') }}"></script>
   <script src="{{ asset('ecoland/js/bootstrap.min.js') }}"></script>
-  <script src="{{ asset('ecoland/js/jquery.easing.1.3.js') }}"></script>
+  {{-- <script src="{{ asset('ecoland/js/jquery.easing.1.3.js') }}"></script> --}}
   <script src="{{ asset('ecoland/js/jquery.waypoints.min.js') }}"></script>
   <script src="{{ asset('ecoland/js/jquery.stellar.min.js') }}"></script>
   <script src="{{ asset('ecoland/js/owl.carousel.min.js') }}"></script>
   <script src="{{ asset('ecoland/js/jquery.magnific-popup.min.js') }}"></script>
   <script src="{{ asset('ecoland/js/aos.js') }}"></script>
-  <script src="{{ asset('ecoland/js/jquery.animateNumber.min.js') }}"></script>
+  {{-- <script src="{{ asset('ecoland/js/jquery.animateNumber.min.js') }}"></script> --}}
   <script src="{{ asset('ecoland/js/scrollax.min.js') }}"></script>
-  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  {{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
   <script src="{{ asset('ecoland/js/google-map.js') }}"></script>
   
   <script src="{{ asset('ecoland/js/main.js') }}"></script>
