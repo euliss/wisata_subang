fb7e5c03bec0fb3e415148230eec8bf7ecbf87c5
abdulrizky <58355002+abdulrizky@users.noreply.github.com>
2023-06-19 22:28:53 +0700

    Update CategoriesSeeder.php

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\M_Categories;
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,31 +15,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        M_Categories::create([
+        DB::table('categories')->create([
             'categories_name' => 'Wista ',
             'images' => 'images/icon/Wisata.png',
             'description' => 'Terdiri dari wisata alam, wisata sejarah, dan lainnya',
             'status' => 'Aktif',
         ]);
-        M_Categories::create([
+        DB::table('categories')->create([
             'categories_name' => 'Kuliner',
             'images' => 'images/icon/Kuliner.png',
             'description' => 'Menyajikan referensi kuliner, mulai dari street food, cafe, rumah makan, da resto',
             'status' => 'Aktif',
         ]);
-        M_Categories::create([
+        DB::table('categories')->create([
             'categories_name' => 'Hotel',
             'images' => 'images/icon/Hotel.png',
             'description' => 'Rekomendasi penginapan hotel, vila, dan home stay',
             'status' => 'Aktif',
         ]);
-        M_Categories::create([
+        DB::table('categories')->create([
             'categories_name' => 'Desa Wisata',
             'images' => 'images/icon/DesaWisata.png',
             'description' => 'Referensi ekonomi kreatif asli Subang untuk oleh-oleh liburanmu',
             'status' => 'Aktif',
         ]);
-        M_Categories::create([
+        DB::table('categories')->create([
             'categories_name' => 'Ekonomi Kreatif',
             'images' => 'images/icon/Ekraf.png',
             'description' => 'Mengenal dan belajar kebudayaan yang ada di Subang',
